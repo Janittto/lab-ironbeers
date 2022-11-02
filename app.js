7f40a2960923ea99d0573aa11cd3244b2ee1b486
jeanne <jeanne.lefevre@hotmail.fr>
2022-11-02 17:56:46 +0100

    lab solved no css

--- app.js
+++ app.js
@@ -13,13 +13,37 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'index'
+  });
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => res.render('beers', { beers }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => res.render('random-beers', beer[0]))
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/beer:id', (req, res) => {
+  console.log(req.params);
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beer => res.render('beer', beer[0]))
+    .catch(error => console.log(error));
+});
+
+app.listen(3000, () => console.log('http://localhost:3000 🏃‍ on port 3000'));
